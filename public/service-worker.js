7c8930593bc5c9d548e7e51fc499addd4e3e70a5
Sean Kirkpatrick <Skirkp18@gmail.com>
2020-12-09 19:14:18 -0500

    fixed issues with manifest

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,1 +1,8 @@
-console.log("Hello from service worker!")
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/favicon.ico",
+  "/manifest.webmanifest",
+];
