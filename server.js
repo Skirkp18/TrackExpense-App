f8de0dc5dd9bd95b84883facc6967035cb88205b
Sean Kirkpatrick <Skirkp18@gmail.com>
2020-12-12 14:59:13 -0500

    fixed port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = process.env.MONGODB_URI || 3000;
 
 const app = express();
 
@@ -28,6 +28,10 @@ mongoose.connect(
 
 // routes
 app.use(require("./routes/api.js"));
+
+app.get("/", function(req, res) {
+  res.send("index.html")
+})
 
 app.listen(PORT, () => {
   console.log(`App running on port http://localhost:${PORT}/`);
